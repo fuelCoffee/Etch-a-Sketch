2d395eeefccf502a54f6d3260fd9881b82e9de8e
fuelCoffee <fuelcoffee@users.noreply.github.com>
2023-11-26 15:53:06 -0600

    Add button onclick functionality to change colors

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+"use strict";
+
+let color = "black";
+
 function populateBoard(size) {
   let board = document.querySelector(".board");
   let squares = document.querySelectorAll("div");
@@ -9,11 +13,8 @@ function populateBoard(size) {
   let amount = size * size;
   for (let i = 0; i < amount; i++) {
     let square = document.createElement("div");
-    square.addEventListener(
-      "mouseover",
-      () => (square.style.backgroundColor = "black")
-    );
-    square.style.backgroundColor = "blue";
+    square.addEventListener("mouseover", colorSquare);
+    square.style.backgroundColor = "white";
     board.insertAdjacentElement("beforeend", square);
   }
 }
@@ -26,4 +27,16 @@ function changeSize(input) {
   } else {
     console.log("too many squares");
   }
+}
+
+function colorSquare() {
+  if (color === "random") {
+    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+  } else {
+    this.style.backgroundColor = color;
+  }
+}
+
+function changeColor(choice) {
+  color = choice;
 }
